572951cf0d7aec3077e15221de00ba4ca32b39ef
maldonadodev <mathiasmaldonado96@gmail.com>
2025-01-22 19:21:12 -0300

    string

---
+++ uso_del_string.js
@@ -0,0 +1,13 @@
+const comillaSimple ='comilla simple';
+const comillaDoble ="comilla doble";
+
+//template string
+const tmp = `vamos a construir un parrafo usando las otras 
+ya definidas. Estas son ${comillaSimple} y ${comillaDoble}
+`;
+
+ const practica1 = "A veces me siento muy fotosintesis 'hee'";
+ const practica2 = 'Epa, "puchero"';
+
+ console.log(tmp);
+ console.log(practica1);
